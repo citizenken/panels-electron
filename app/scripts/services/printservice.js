71f91c3fbf608e3fa8176cec5470c35de57f0a8c
Ken Petti <kennethpetti@gmail.com>
2017-04-01 10:02:13 -0400

    improving printing, disabling sync if not logged in

--- app/scripts/services/printservice.js
+++ app/scripts/services/printservice.js
@@ -21,6 +21,7 @@ angular.module('panels')
       print: function () {
         var self = this,
         BrowserWindow = require('electron').remote.BrowserWindow,
+        fs = require('fs'),
         url = encodeURI(self.getCss() + self.getHtml()),
         fullUrl = self.rootUrl + url;
 
@@ -28,25 +29,72 @@ angular.module('panels')
 
         printWindow.loadURL(fullUrl);
         printWindow.once('ready-to-show', function () {
-          try {
-            printWindow.webContents.print();
-          } catch (err) {
-            console.log('caught show error', err);
-          }
+          printWindow.webContents.print();
+          // try {
+          //   printWindow.webContents.printToPDF({}, function (error, data) {
+          //     if (error) throw error
+          //     fs.writeFile('/tmp/print.pdf', data, (error) => {
+          //       if (error) throw error
+          //       console.log('Write PDF successfully.')
+          //     })
+          //   });
+          // } catch (err) {
+          //   console.log('caught show error', err);
+          // }
         });
-        
-
       },
-      
+
       getHtml: function () {
-        return angular.element('.page')[0].outerHTML;
+        var self = this,
+        pages = self.makePages(angular.copy(angular.element('.page')[0])).join("");
+        return pages;
+      },
+
+      makePages: function (page) {
+        var self = this,
+        titles = angular.element('.cm-vlc-title-wrapper'),
+        pages = [];
+
+        for (var i = 0; i < titles.length; i++) {
+          pages.push(angular.copy(page));
+        }
+
+        angular.forEach(pages, function (p, index) {
+          var lines = Array.prototype.slice.call(p.getElementsByClassName('CodeMirror-line')),
+          codeEl = p.getElementsByClassName('CodeMirror-code')[0],
+          titles = p.getElementsByClassName('cm-vlc-title-wrapper'),
+          startTitle = titles[index],
+          startIndex = Array.prototype.indexOf.call(lines, startTitle),
+          finishTitle = null,
+          finishIndex = null;
+
+          if (titles.length > (index + 1)) {
+            finishTitle = titles[index + 1]
+            finishIndex = Array.prototype.indexOf.call(lines, finishTitle);
+          }
+
+          // console.log(startIndex, finishIndex);
+
+          for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            if (i < startIndex || finishIndex && i >= finishIndex) {
+              // console.log(i);
+              line.parentNode.removeChild(line);
+            }
+          }
+
+
+          pages[index] = p.outerHTML;
+        });
+
+        return pages;
       },
 
       getCss: function () {
         var styleSheets = document.styleSheets,
         rules = [];
         angular.forEach(styleSheets, function (style) {
-          if (style.href && 
+          if (style.href &&
             (style.href.indexOf('styles/') > -1 || style.href.indexOf('codemirror.css') > -1)) {
             var styleTag = '<style>';
             angular.forEach(style.rules, function (rule) {
@@ -54,7 +102,7 @@ angular.module('panels')
             });
             styleTag += '</style>';
             rules.unshift(styleTag);
-            
+
           }
         });
         return rules.join('');
